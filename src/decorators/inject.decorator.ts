edf5e04568b73f19e408ae7605e890b9dba25aa4
Vladislav Dedovich <cogee@ya.ru>
2023-02-12 14:32:37 +0100

    add constructor based injection

--- src/decorators/inject.decorator.ts
+++ src/decorators/inject.decorator.ts
@@ -1,13 +1,14 @@
 import { type Constructable } from '../types/constructable.type'
-import { definePropertyResolver, getPropertyResolver } from '../utils'
+import { defineResolver, getResolver } from '../utils'
 
 type InjectFactory = () => Constructable
+type InjectReturnType = (target: Object, propertyKey: string | symbol, parameterIndex?: number) => void
 
-export function Inject (token?: string | symbol): PropertyDecorator
-export function Inject (factory?: InjectFactory): PropertyDecorator
-export function Inject (tokenOrFactory?: string | symbol | InjectFactory): PropertyDecorator {
-  return (target, propertyKey) => {
-    const propertyResolver = getPropertyResolver(target, propertyKey, tokenOrFactory)
-    definePropertyResolver(target, propertyResolver)
+export function Inject (token?: string | symbol): InjectReturnType
+export function Inject (factory?: InjectFactory): InjectReturnType
+export function Inject (tokenOrFactory?: string | symbol | InjectFactory): InjectReturnType {
+  return (target, propertyKey, index) => {
+    const resolver = getResolver(target, propertyKey, index, tokenOrFactory)
+    defineResolver(target, resolver)
   }
 }
